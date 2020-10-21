455ef4704572dad5a01c0536f1d1ba085810ec7c
majkel84 <michal.sobotka.84@gmail.com>
2020-10-21 22:45:02 +0200

    make_unique - test for int, double, bool, objects

--- module1/homework/make_unique/make_unique-ut.cpp
+++ module1/homework/make_unique/make_unique-ut.cpp
@@ -3,13 +3,59 @@
 #include "make_unique.hpp"
 
 constexpr int testIntValue = 10;
+constexpr double testDoubleValue = 3.14;
+constexpr bool testBoolValue = true;
+constexpr size_t arraySize = 5;
 
-struct make_uniqueTest : public ::testing::Test {
+template<class Type> struct default_delete;
+template<class Type> struct default_delete<Type[]>;
+
+struct make_uniqueTest {
+public:
     make_uniqueTest();
-    std::unique_ptr<int> uPtr = std::make_unique<int>(testIntValue);
+    make_uniqueTest(int intValue, double doubleValue, bool boolValue)
+    : intValue_(intValue), doubleValue_(doubleValue), boolValue_(boolValue)
+    {}
+
+    bool operator==(const make_uniqueTest& other) const {
+        return intValue_ == other.intValue_
+            && doubleValue_ == other.doubleValue_
+            && boolValue_ == other.boolValue_;
+    }
+
+private:
+    int intValue_;
+    double doubleValue_;
+    bool boolValue_;
 };
 
-TEST(make_uniqueTest, testMakeUniqueForIntValue) {
+TEST(make_uniqueIntTest, testMakeUniqueForIntValue) {
     std::unique_ptr<int> uPtr = cs::make_unique<int>(testIntValue);
     ASSERT_EQ(*uPtr, testIntValue);
+}
+
+TEST(make_uniqueArrayTest, testMakeUniqueForArrayType) {
+    std::unique_ptr<int[]> uPtr = cs::make_unique<int[]>(arraySize);
+}
+
+TEST(make_uniqueEmptyTest, testIntEmptyConstruct) {
+    std::unique_ptr<int> uniqZero = cs::make_unique<int>();
+    ASSERT_EQ(*uniqZero, 0);
+}
+
+TEST(make_uniqueLvalueTest, testLvalueConstruct) {
+    int lvalue{2020};
+    auto uniqTwenty = cs::make_unique<int>(lvalue);
+    ASSERT_EQ(*uniqTwenty, lvalue);
+}
+
+TEST(make_uniqueCustomObjectTest, testLvalueDoubleBoolConstruct) {
+    make_uniqueTest muTest{testIntValue, testDoubleValue, testBoolValue};
+    auto uniqeObject = cs::make_unique<make_uniqueTest>(testIntValue, testDoubleValue, testBoolValue);
+    ASSERT_EQ(*uniqeObject, muTest);
+
+    int lvalue{2020};
+    make_uniqueTest muTest2{lvalue, testDoubleValue, testBoolValue};
+    auto uniqeObject2 = cs::make_unique<make_uniqueTest>(lvalue, testDoubleValue, testBoolValue);
+    ASSERT_EQ(*uniqeObject2, muTest2);
 }
--- module1/homework/make_unique/make_unique.hpp
+++ module1/homework/make_unique/make_unique.hpp
@@ -2,12 +2,27 @@
 
 #include <memory>
 #include <utility>
+#include <type_traits>
+
+template <typename Type>
+using isUnBounded = std::enable_if_t<!std::is_array_v<Type>>;
 
 namespace cs {
 
+// template <typename Type, class... Args, typename = isUnBounded<Type>>
+// std::unique_ptr<Type> make_unique(Args&&... args) {
+//     return std::unique_ptr<Type>(new Type(std::forward<Args>(args)...));
+// }
+
 template <typename Type, class... Args>
-std::unique_ptr<Type> make_unique(Args&&... args) {
+std::enable_if_t<!std::is_array_v<Type>, std::unique_ptr<Type>>
+make_unique(Args&&... args) {
     return std::unique_ptr<Type>(new Type(std::forward<Args>(args)...));
+}
+
+template<typename Type>
+std::unique_ptr<Type> make_unique(std::size_t size) {
+    return std::unique_ptr<Type>(new typename std::remove_extent<Type>::type[size]());
 }
 
 } //namespace cs
