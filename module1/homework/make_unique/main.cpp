98b6eabd3376b1c389b41595b3140a524d79e8f6
jakjan95 <jakubjanusz95@gmail.com>
2020-10-24 21:34:11 +0200

    make_unique works and passing all tests

--- module1/homework/make_unique/main.cpp
+++ module1/homework/make_unique/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <memory>
+
+#include "make_unique.hpp"
 
 int main() {
-    std::cout << "test\n";
+    auto ptr = cs::make_unique<int>(5);
+    std::cout<<*ptr<<'\n';
     return 0;
 }
--- module1/homework/make_unique/tests/make_unique.ut.cpp
+++ module1/homework/make_unique/tests/make_unique.ut.cpp
@@ -1,6 +1,47 @@
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+
+#include <iostream>
+
 #include "make_unique.hpp"
 
-TEST(TestFirst, ShouldReturnTrue) {
-    EXPECT_TRUE(true);
+constexpr int defaultValuesOfTestClass = 0;
+constexpr int initialValuesOfTestClass = 2;
+constexpr int initialValue = 1;
+constexpr int secondValue = 2;
+
+class TestClass {
+public:
+    TestClass(int value = defaultValuesOfTestClass)
+        : value_{value} {}
+
+    int getValue() const {
+        return value_;
+    }
+
+private:
+    int value_;
+};
+
+TEST(MakeUniqueTest, ShouldCreateUniquePointerFromInt) {
+    auto ptr = cs::make_unique<int>(initialValue);
+    ASSERT_EQ(*ptr, initialValue);
+}
+
+TEST(MakeUniqueTest, ShouldCreateUniquePointerFromCustomClassWithDefaultValue) {
+    auto testClass = cs::make_unique<TestClass>();
+    ASSERT_EQ(testClass->getValue(), defaultValuesOfTestClass);
+}
+
+TEST(MakeUniqueTest, ShouldCreateUniquePointerFromCustomClassWithInitialValue) {
+    auto testClass = cs::make_unique<TestClass>(initialValuesOfTestClass);
+    ASSERT_EQ(testClass->getValue(), initialValuesOfTestClass);
+}
+
+TEST(MakeUniqueTest, ShouldCreateUniquePointerForArrayType) {
+    constexpr int arraySize = 2;
+    auto arrayPtr = cs::make_unique<int[]>(arraySize);
+    arrayPtr[0] = initialValue;
+    arrayPtr[1] = secondValue;
+    ASSERT_EQ(arrayPtr[0], initialValue);
+    ASSERT_EQ(arrayPtr[1], secondValue);
 }
