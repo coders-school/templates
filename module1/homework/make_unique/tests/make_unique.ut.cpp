5a0385935422b44b678e49a1b1fda6e4b8ce4f6c
jakjan95 <jakubjanusz95@gmail.com>
2020-10-29 17:41:16 +0100

    Added test cases for multiple parameters

--- module1/homework/make_unique/tests/make_unique.ut.cpp
+++ module1/homework/make_unique/tests/make_unique.ut.cpp
@@ -6,20 +6,38 @@
 
 constexpr int defaultValuesOfTestClass = 0;
 constexpr int initialValuesOfTestClass = 2;
+constexpr int initialValuesForSecondPrameterOfTestClass = 6;
 constexpr int initialValue = 1;
 constexpr int secondValue = 2;
 
+constexpr int leftValueConstructor = 69;
+constexpr int rightValueConstructor = 96;
+
 class TestClass {
 public:
-    TestClass(int value = defaultValuesOfTestClass)
-        : value_{value} {}
+    TestClass(int firstValue = defaultValuesOfTestClass)
+        : firstValue_{firstValue} {}
 
-    int getValue() const {
-        return value_;
+    TestClass(int firstValue, int secondValue)
+        : firstValue_{firstValue}, secondValue_{secondValue} {}
+
+    int firstValue_;
+    int secondValue_{defaultValuesOfTestClass};
+};
+
+struct SideValueTestClass {
+    SideValueTestClass(int& val)
+        : classValue_{val} {
+        sideTestValue_ = leftValueConstructor;
     }
 
-private:
-    int value_;
+    SideValueTestClass(int&& val)
+        : classValue_{val} {
+        sideTestValue_ = rightValueConstructor;
+    }
+
+    int classValue_;
+    int sideTestValue_;
 };
 
 TEST(MakeUniqueTest, ShouldCreateUniquePointerFromInt) {
@@ -29,12 +47,22 @@ TEST(MakeUniqueTest, ShouldCreateUniquePointerFromInt) {
 
 TEST(MakeUniqueTest, ShouldCreateUniquePointerFromCustomClassWithDefaultValue) {
     auto testClass = cs::make_unique<TestClass>();
-    ASSERT_EQ(testClass->getValue(), defaultValuesOfTestClass);
+    ASSERT_EQ(testClass->firstValue_, defaultValuesOfTestClass);
+    ASSERT_EQ(testClass->secondValue_, defaultValuesOfTestClass);
 }
 
 TEST(MakeUniqueTest, ShouldCreateUniquePointerFromCustomClassWithInitialValue) {
     auto testClass = cs::make_unique<TestClass>(initialValuesOfTestClass);
-    ASSERT_EQ(testClass->getValue(), initialValuesOfTestClass);
+    ASSERT_EQ(testClass->firstValue_, initialValuesOfTestClass);
+    ASSERT_EQ(testClass->secondValue_, defaultValuesOfTestClass);
+}
+
+TEST(MakeUniqueTest, ShouldCreatedUniquePointerFromCustomClassWithTwoParameters) {
+    auto testClass = cs::make_unique<TestClass>(initialValuesOfTestClass,
+                                                initialValuesForSecondPrameterOfTestClass);
+
+    ASSERT_EQ(testClass->firstValue_, initialValuesOfTestClass);
+    ASSERT_EQ(testClass->secondValue_, initialValuesForSecondPrameterOfTestClass);
 }
 
 TEST(MakeUniqueTest, ShouldCreateUniquePointerForArrayType) {
@@ -45,3 +73,10 @@ TEST(MakeUniqueTest, ShouldCreateUniquePointerForArrayType) {
     ASSERT_EQ(arrayPtr[0], initialValue);
     ASSERT_EQ(arrayPtr[1], secondValue);
 }
+
+// TEST(MakeUniqueTest, ShouldCreatePointerUsingLValue) {
+//     constexpr int leftValueConstructorTest = 5;
+//     auto testClass = cs::make_unique<SideValueTestClass>(leftValueConstructorTest);
+
+//     ASSERT_EQ(testClass->sideTestValue_, leftValueConstructor);
+// }
