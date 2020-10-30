5d44ea1c8dd4b19d3b7d6fefaafae63dd6459d17
Vicx95 <wiktor.zyla31@gmail.com>
2020-10-30 21:43:28 +0100

    Add some test code to main

--- module1/homework/make_unique/main.cpp
+++ module1/homework/make_unique/main.cpp
@@ -1,7 +1,18 @@
+#include <iostream>
+#include "make_unique.hpp"
 
+struct MyType {
+    MyType(int&, double&&, bool) { std::cout << "lvalue, rvalue, copy\n"; }
+    MyType(int&&, double&, bool) { std::cout << "rvalue, lvalue, copy\n"; }
+};
 
-
-int main(){
-    
+int main() {
+    int lvalue{2020};
+    std::unique_ptr<int> uniqZero = MyMakeUnique::make_unique<int>();
+    auto uniqEleven = MyMakeUnique::make_unique<int>(2011);
+    auto uniqTwenty = MyMakeUnique::make_unique<int>(lvalue);
+    auto uniqTypeLRC = MyMakeUnique::make_unique<MyType>(lvalue, 3.14, true);
+    double PI = 3.14;
+    auto uniqTypeRRC = MyMakeUnique::make_unique<MyType>(2020, PI, true);
     return 0;
 }
