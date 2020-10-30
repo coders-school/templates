736704994c29c53525c913fbda4f05ad055453a0
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-10-30 13:52:52 +0100

    make unique done with tests

--- module1/homework/make_unique/main.cpp
+++ module1/homework/make_unique/main.cpp
@@ -5,14 +5,14 @@
 struct MyType {
     MyType(int&, double&&, bool) { std::cout << "lvalue, rvalue, copy\n"; }
     MyType(int&&, double&, bool) { std::cout << "rvalue, lvalue, copy\n"; }
+    MyType(int&&, double&&, bool) { std::cout << "rvalue, rvalue, copy\n"; }
 };
 
 int main() {
     int lvalue{2020};
-    std::cout << lvalue << '\n';
-    // std::unique_ptr<int> uniqZero = make_unique<int>();
-    // auto uniqEleven = make_unique<int>(2011);
-    // auto uniqTwenty = make_unique<int>(lvalue);
-    // auto uniqTypeLRC = make_unique<MyType>(lvalue, 3.14, true);
-    // auto uniqTypeRRC = make_unique<MyType>(2020, 3.14, true);
+    std::unique_ptr<int> uniqZero = coders::make_unique<int>();
+    auto uniqEleven = coders::make_unique<int>(2011);
+    auto uniqTwenty = coders::make_unique<int>(lvalue);
+    auto uniqTypeLRC = coders::make_unique<MyType>(lvalue, 3.14, true);
+    auto uniqTypeRRC = coders::make_unique<MyType>(2020, 3.14, true);
 }
--- module1/homework/make_unique/make_unique-ut.cpp
+++ module1/homework/make_unique/make_unique-ut.cpp
@@ -1,47 +1,95 @@
-#include <gtest/gtest.h>
-
+#include <array>
+#include <memory>
+#include "gtest/gtest.h"
 #include "make_unique.hpp"
 
-// constexpr int testValue = 10;
-// constexpr int secondTestValue = 666;
+constexpr int testValInt{666};
+constexpr double testValDbl{123.456};
+constexpr int testValInt_L{50};
+constexpr int testValInt_R{60};
 
-// struct MakeUniqueTest : ::testing::Test {
-//     SharedPointerTest()
-//         : sharedPtr(new int(testValue)) {}
-//     coders::shared_ptr<int> sharedPtr;
-// };
+struct TestStruct {
+    int structValInt_{};
+    double structValDbl_{};
 
-// TEST_F(SharedPointerTest, ShouldCreateNullptrSharedPointerToIntegerUsingDefaultConstructor) {
-//     // Given
-//     size_t sharedPtrCnt = 1;
+    TestStruct() = default;
+    TestStruct(int i_val, double d_val)
+        : structValInt_(i_val), structValDbl_(d_val) {}
+};
 
-//     // When
-//     coders::shared_ptr<int> emptySharedPtr{};
+struct LRvalTestStruct {
+    int testValue;
 
-//     // Then
-//     ASSERT_FALSE(emptySharedPtr);
-//     ASSERT_EQ(emptySharedPtr.get(), nullptr);
-//     ASSERT_EQ(emptySharedPtr.operator->(), nullptr);
-//     ASSERT_EQ(emptySharedPtr.use_count(), sharedPtrCnt);
-// }
+    LRvalTestStruct(int val = testValInt_L)
+        : testValue{val} {}
 
-// TEST(SharedPointerTestWithDeleter, ShouldUseGivenDeleter) {
-//     // Given
-//     struct testStruct {
-//         int a = 20;
-//     };
-//     testStruct* tStruct = new testStruct();
-//     auto testDeleter = [](testStruct* p) {
-//         std::cout << "My deleter!\n";
-//         p->a = -1;
-//     };
+    LRvalTestStruct(const LRvalTestStruct&)
+        : testValue{testValInt_L} {}
 
-//     // When
-//     {
-//         coders::shared_ptr<testStruct> testPtr(tStruct, testDeleter);
-//     }
+    LRvalTestStruct(LRvalTestStruct&&)
+        : testValue{testValInt_R} {}
+};
 
-//     // Then
-//     ASSERT_EQ(tStruct->a, -1);
-//     delete tStruct;
-// }
+TEST(MakeUniqueTest, ShouldMakeUniqueForInt) {
+    auto uniquePtr = coders::make_unique<int>(testValInt);
+    ASSERT_EQ(*uniquePtr, testValInt);
+}
+
+TEST(MakeUniqueTest, ShouldMakeUniqueForEmptyInt) {
+    auto uniquePtr = coders::make_unique<int>();
+    ASSERT_EQ(*uniquePtr, 0);
+}
+
+TEST(MakeUniqueTest, ShouldMakeUniqueForDbl) {
+    auto uniquePtr = coders::make_unique<double>(testValDbl);
+    ASSERT_EQ(*uniquePtr, testValDbl);
+}
+
+TEST(MakeUniqueTest, ShouldMakeUniqueForIntArray) {
+    int arrSize{10};
+    int offset = 5;
+    auto uniquePtr = coders::make_unique<int[]>(arrSize);
+    for (auto i = 0; i < arrSize; i++) {
+        uniquePtr[i] = i + offset;
+    }
+    for (auto i = 0; i < arrSize; i++) {
+        ASSERT_EQ(uniquePtr[i], i + offset);
+    }
+}
+
+TEST(MakeUniqueTest, ShouldMakeUniqueForSTLIntArray) {
+    constexpr int arrSize{4};
+    std::array<int, arrSize> stlIntArray = {6, 66, 666, 6666};
+    auto uniquePtr = coders::make_unique<int[]>(arrSize);
+    for (auto i = 0; i < arrSize; i++) {
+        uniquePtr[i] = stlIntArray[i];
+    }
+    for (auto i = 0; i < arrSize; i++) {
+        ASSERT_EQ(uniquePtr[i], stlIntArray[i]);
+    }
+}
+
+TEST(MakeUniqueTest, ShouldMakeUniqueForTestStruct) {
+    auto uniquePtr = coders::make_unique<TestStruct>(testValInt, testValDbl);
+
+    ASSERT_EQ(uniquePtr->structValInt_, testValInt);
+    ASSERT_EQ(uniquePtr->structValDbl_, testValDbl);
+}
+
+TEST(MakeUniqueTest, ShouldMakeUniqueForEmptyTestStruct) {
+    auto uniquePtr = coders::make_unique<TestStruct>();
+
+    ASSERT_EQ(uniquePtr->structValInt_, 0);
+    ASSERT_EQ(uniquePtr->structValDbl_, 0.0);
+}
+
+TEST(MakeUniqueTest, ShouldMakeUniqueForLvalTestStruct) {
+    auto uniquePtr = coders::make_unique<LRvalTestStruct>();
+    ASSERT_EQ(uniquePtr->testValue, testValInt_L);
+}
+
+TEST(MakeUniqueTest, ShouldMakeUniqueForRvalTestStruct) {
+    LRvalTestStruct temp;
+    auto uniquePtr = coders::make_unique<LRvalTestStruct>(std::move(temp));
+    ASSERT_EQ(uniquePtr->testValue, testValInt_R);
+}
