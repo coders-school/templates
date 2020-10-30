04f97fc7b9d52e6865ebbe14a658c2b7e424eda5
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-10-30 14:10:01 +0100

    add more tests

--- module1/homework/make_unique/make_unique-ut.cpp
+++ module1/homework/make_unique/make_unique-ut.cpp
@@ -5,29 +5,44 @@
 
 constexpr int testValInt{666};
 constexpr double testValDbl{123.456};
+
 constexpr int testValInt_L{50};
 constexpr int testValInt_R{60};
+constexpr double testValDbl_L{150.150};
+constexpr double testValDbl_R{160.160};
 
 struct TestStruct {
-    int structValInt_{};
-    double structValDbl_{};
-
     TestStruct() = default;
     TestStruct(int i_val, double d_val)
         : structValInt_(i_val), structValDbl_(d_val) {}
+
+    int structValInt_{};
+    double structValDbl_{};
 };
 
 struct LRvalTestStruct {
-    int testValue;
-
     LRvalTestStruct(int val = testValInt_L)
-        : testValue{val} {}
+        : testValue_{val} {}
 
     LRvalTestStruct(const LRvalTestStruct&)
-        : testValue{testValInt_L} {}
+        : testValue_{testValInt_L} {}
 
     LRvalTestStruct(LRvalTestStruct&&)
-        : testValue{testValInt_R} {}
+        : testValue_{testValInt_R} {}
+
+    int testValue_;
+};
+
+struct MyType {
+    MyType() = default;
+    MyType(int&, double&&, bool)
+        : structValInt_(testValInt_L), structValDbl_(testValDbl_R), structValBool_(true) {}
+    MyType(int&&, double&, bool)
+        : structValInt_(testValInt_R), structValDbl_(testValDbl_L), structValBool_(true) {}
+
+    int structValInt_{testValInt};
+    double structValDbl_{testValDbl};
+    bool structValBool_{false};
 };
 
 TEST(MakeUniqueTest, ShouldMakeUniqueForInt) {
@@ -85,11 +100,27 @@ TEST(MakeUniqueTest, ShouldMakeUniqueForEmptyTestStruct) {
 
 TEST(MakeUniqueTest, ShouldMakeUniqueForLvalTestStruct) {
     auto uniquePtr = coders::make_unique<LRvalTestStruct>();
-    ASSERT_EQ(uniquePtr->testValue, testValInt_L);
+    ASSERT_EQ(uniquePtr->testValue_, testValInt_L);
 }
 
 TEST(MakeUniqueTest, ShouldMakeUniqueForRvalTestStruct) {
     LRvalTestStruct temp;
     auto uniquePtr = coders::make_unique<LRvalTestStruct>(std::move(temp));
-    ASSERT_EQ(uniquePtr->testValue, testValInt_R);
+    ASSERT_EQ(uniquePtr->testValue_, testValInt_R);
+}
+
+TEST(MakeUniqueTest, ShouldMakeUniqueForMyTypeWithProperConstructor) {
+    int testInt{9};
+    auto uniquePtr = coders::make_unique<MyType>(testInt, 0.0, false);
+
+    ASSERT_EQ(uniquePtr->structValInt_, testValInt_L);
+    ASSERT_EQ(uniquePtr->structValDbl_, testValDbl_R);
+    ASSERT_EQ(uniquePtr->structValBool_, true);
+
+    double testDbl{99.99};
+    auto another_uniquePtr = coders::make_unique<MyType>(0, testDbl, false);
+
+    ASSERT_EQ(another_uniquePtr->structValInt_, testValInt_R);
+    ASSERT_EQ(another_uniquePtr->structValDbl_, testValDbl_L);
+    ASSERT_EQ(another_uniquePtr->structValBool_, true);
 }
