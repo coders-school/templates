135e6607d54806d369839fdb7c40980ee6c95e3b
WojtekMs <wojtek.atmo@gmail.com>
2020-11-02 18:12:26 +0100

    perfect forwarding checks added

--- module1/homework/make_unique/tests/tests.cpp
+++ module1/homework/make_unique/tests/tests.cpp
@@ -1,6 +1,7 @@
-#include <gtest/gtest.h>
-
 #include "make_unique.hpp"
+
+#include <gtest/gtest.h>
+#include <sstream>
 
 struct MyType {
     MyType(int&, double&&, bool) { std::cout << "lvalue, rvalue, copy\n"; }
@@ -9,26 +10,53 @@ struct MyType {
 
 class makeUniqueTests : public ::testing::Test
 {
+    std::streambuf* coutBuffer{};
+
    protected:
     int defaultValue{5};
     double anotherValue{10.5};
+
+    std::stringstream testStream{};
+
+    void SetUp() override
+    {
+        coutBuffer = std::cout.rdbuf();
+        std::cout.rdbuf(testStream.rdbuf());
+    }
+
+    void TearDown() override
+    {
+        std::cout.rdbuf(coutBuffer);
+        testStream.ignore(std::numeric_limits<std::streamsize>::max());
+    }
 };
 
-TEST_F(makeUniqueTests, noParametersShouldBeDefaultObject) {
+TEST_F(makeUniqueTests, noParametersShouldBeDefaultObject)
+{
     auto uniqueZero = cs::make_unique<int>();
     EXPECT_EQ(*uniqueZero, 0);
 }
 
-TEST_F(makeUniqueTests, shouldCreateWithGivenValue) {
+TEST_F(makeUniqueTests, shouldCreateWithGivenValue)
+{
     auto uniqueZero = cs::make_unique<int>(defaultValue);
     EXPECT_EQ(*uniqueZero, defaultValue);
-        
+
     auto uniqueOne = cs::make_unique<double>(anotherValue);
     EXPECT_DOUBLE_EQ(*uniqueOne, anotherValue);
 }
 
-TEST_F(makeUniqueTests, shouldCreateCustomObject) {
+TEST_F(makeUniqueTests, shouldCreateCustomObject)
+{
+    auto expectedLRCOutput{"lvalue, rvalue, copy\n"};
     auto uniqueLRC = cs::make_unique<MyType>(defaultValue, 5.0, true);
-    auto uniqueRLC = cs::make_unique<MyType>(3, anotherValue, false);
-}
+    auto actualLRCOutput{testStream.str()};
+    EXPECT_EQ(expectedLRCOutput, actualLRCOutput);
+    
+    testStream.str(std::string());
 
+    auto expectedRLCOutput{"rvalue, lvalue, copy\n"};
+    auto uniqueRLC = cs::make_unique<MyType>(3, anotherValue, false);
+    auto actualRLCOutput{testStream.str()};
+    EXPECT_EQ(expectedRLCOutput, actualRLCOutput);
+}
